2390521996ac2fdc970035fcd4377457db980ec9
David Cohen <david.cohen@haloinvesting.com>
2019-08-20 11:26:54 -0500

    Added tradable data download link

--- static/js/table.js
+++ static/js/table.js
@@ -1,12 +1,45 @@
+$(function() {
 
-$('table thead th').on('click', event => {
-    var target = $(event.target);
+    var bindTableDownload = (table, link) => {
+        // Binds a download-table action on the given link for the given table
 
-    // Get the index of the clicked tablehead cell:
-    var cells = target.closest('tr').find('th');
-    var index = cells.map((index, cell) => $(cell).text() == target.text() ? index : null).filter((_, item) => item != null);
+        var csvcontent = 'data:text/csv;charset=utf-8,';
 
+        // Loop through table headers:
+        table.find('thead > tr').each((i, header) => {
+            // Loop through table header cells:
+            $(header).find('th').each((i, cell) => {
+                // Append cell contents:
+                csvcontent += $(cell).text().trim() + ',';
+
+                // Add extra spacing for colspans:
+                var colspan = $(cell).attr('colspan');
+                if (colspan) {
+                    var count = parseInt(colspan) - 1;
+                    csvcontent += ','.repeat(count);
+                }
+            });
+            csvcontent += '\r\n';
+        });
+
+        // Get Table Content:
+        table.find('tbody > tr').each((i, row) => {
+            $(row).find('td').each((i, cell) => {
+                // Append cell contents:
+                csvcontent += $(cell).text().trim() + ',';
+
+            });
+            csvcontent += '\r\n';
+        });
+
+        // Chrome, Firefox: Create and Click hidden download link:
+        link.attr({
+            href: encodeURI(csvcontent),
+            download: 'data.csv',
+        });
+    }
 
     var table = $('table');
-
+    var button = $('.download');
+    bindTableDownload(table, button);
 });
